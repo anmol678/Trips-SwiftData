d4abf9a39799f0ac580128dbb66cf714cc9bc6de
Anmol Jain <anmolj678@gmail.com>
2024-08-01 13:36:09 -0700

    initial commit

---
+++ Trips/ContentView.swift
@@ -0,0 +1,99 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A SwiftUI view that shows the main UI.
+*/
+
+import SwiftUI
+import SwiftData
+import WidgetKit
+
+struct ContentView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var showAddTrip = false
+    @State private var selection: Trip?
+    @State private var searchText: String = ""
+    @State private var tripCount = 0
+    @State private var unreadTripIdentifiers: [PersistentIdentifier] = []
+
+    var body: some View {
+        NavigationSplitView {
+            TripListView(selection: $selection, tripCount: $tripCount,
+                         unreadTripIdentifiers: $unreadTripIdentifiers,
+                         searchText: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                        .disabled(tripCount == 0)
+                }
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Spacer()
+                    Button {
+                        showAddTrip = true
+                    } label: {
+                        Label("Add trip", systemImage: "plus")
+                    }
+                }
+            }
+            #if os(macOS)
+            .navigationSplitViewColumnWidth(min: 200, ideal: 250)
+            #endif
+
+        } detail: {
+            if let selection = selection {
+                NavigationStack {
+                    TripDetailView(trip: selection)
+                }
+            }
+        }
+        .task {
+            let tripIdentifiers = await DataModel.shared.unreadTripIdentifiersInUserDefaults
+            unreadTripIdentifiers = tripIdentifiers
+        }
+        .searchable(text: $searchText, placement: .sidebar)
+        .sheet(isPresented: $showAddTrip) {
+            NavigationStack {
+                AddTripView()
+            }
+            .presentationDetents([.medium, .large])
+        }
+        .onChange(of: selection) { _, newValue in
+            if let newSelection = newValue {
+                if let index = unreadTripIdentifiers.firstIndex(where: {
+                    $0 == newSelection.persistentModelID
+                }) {
+                    unreadTripIdentifiers.remove(at: index)
+                }
+            }
+        }
+        .onChange(of: scenePhase) { _, newValue in
+            Task {
+                if newValue == .active {
+                    unreadTripIdentifiers += await DataModel.shared.findUnreadTripIdentifiers()
+                } else {
+                    // Persist the unread trip identifiers for the next launch session.
+                    let tripIdentifiers = unreadTripIdentifiers
+                    await DataModel.shared.setUnreadTripIdentifiersInUserDefaults(tripIdentifiers)
+                }
+            }
+        }
+        #if os(macOS)
+        .onReceive(NotificationCenter.default.publisher(for: NSApplication.didBecomeActiveNotification)) { _ in
+            Task {
+                unreadTripIdentifiers += await DataModel.shared.findUnreadTripIdentifiers()
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: NSApplication.willTerminateNotification)) { _ in
+            Task {
+                let tripIdentifiers = unreadTripIdentifiers
+                await DataModel.shared.setUnreadTripIdentifiersInUserDefaults(tripIdentifiers)
+            }
+        }
+        #endif
+    }
+}
+
+#Preview(traits: .sampleData) {
+    ContentView()
+}
